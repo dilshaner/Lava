510c5f6ffc645a203747193478acc9ca435323b3
Dilshaner <86090291+dilshaner@users.noreply.github.com>
2024-04-10 20:43:09 +0530

    ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-f67da64f-0403-45d1-a129-b2c3eada72f6/"
 
 OUTPUT_DIR="logs"
 
